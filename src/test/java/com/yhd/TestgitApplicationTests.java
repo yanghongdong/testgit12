102fa68dcfef8e44c6abb3a9360b95137544e30e
yanghongdong <719563903@qq.com>
2021-05-01 02:02:30 +0800

    Initial commit

---
+++ src/test/java/com/yhd/TestgitApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yhd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Hello Git");
+    }
+
+}
